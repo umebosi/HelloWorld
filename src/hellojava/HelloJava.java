68dff75df274ad0adac35c4d95115868f3eadafd
umebosi <ootasyou@s9.dion.ne.jp>
2016-08-11 20:41:48 +0900

    fugaメソッド追加

--- src/hellojava/HelloJava.java
+++ src/hellojava/HelloJava.java
@@ -5,6 +5,13 @@ public class HelloJava {
 	public static void main(String[] args) {
 
 		System.out.println("Hello world!");
+
+		fuga();
+	}
+
+	private static void fuga() {
+
+		System.out.println("Fuga fuga!");
 	}
 
 }
