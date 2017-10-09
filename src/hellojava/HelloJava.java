159da81c32f784ad8a3f960cc7a4134bb3174791
s.oota <topiaoota@yahoo.co.jp>
2017-10-10 00:01:15 +0900

    merge

--- src/hellojava/HelloJava.java
+++ src/hellojava/HelloJava.java
@@ -1,10 +1,13 @@
 package hellojava;
 
+/**
+ *
+ */
 public class HelloJava {
 
 	public static void main(String[] args) {
 
-		System.out.println("**** Hello how are you!!!!! ****");
+		System.out.println("**** Hello how are you!! ****");
 
 		Hi2 hi = new Hi2();
 		hi.howareyou();
